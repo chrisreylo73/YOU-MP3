8e5b8191ee4beb682c310b2fb733a46ba286b4ea
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-24 15:42:28 -0600

    setting up gui

--- youtube2mp3.py
+++ youtube2mp3.py
@@ -2,7 +2,8 @@ import time
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
 import os
-import shutil
+import tkinter as tk
+from tkinter import filedialog
 
 
 def download_youtube_video(url, video_folder_path):
@@ -24,13 +25,47 @@ def delete_video(video_path):
         os.remove(video_path)
     except OSError as e:
         print(f"Error removing file: {e}")
-       
+      
 if __name__ == "__main__":
-    home = "https://www.youtube.com/watch?v=iB3K1f7CFbo"
-    youtube_url = home
-    video_folder_path = "./Videos"
-    videoTitle = download_youtube_video(youtube_url, video_folder_path)
-    output_audio_path = f"./Audio/{videoTitle}.mp3"
-    extract_audio(f"{video_folder_path}/{videoTitle}.mp4", output_audio_path)
-    time.sleep(3)
-    delete_video(f"{video_folder_path}/{videoTitle}.mp4")
+    # home = "https://www.youtube.com/watch?v=iB3K1f7CFbo"
+    # youtube_url = home
+    # video_folder_path = "./Videos"
+    # videoTitle = download_youtube_video(youtube_url, video_folder_path)
+    # output_audio_path = f"./Audio/{videoTitle}.mp3"
+    # extract_audio(f"{video_folder_path}/{videoTitle}.mp4", output_audio_path)
+    # delete_video(f"{video_folder_path}/{videoTitle}.mp4")
+    root = tk.Tk()
+    root.title("YOU MP3")
+    root.geometry(f"{500}x{200}")
+    root.configure(bg="#141414")
+
+    # Create and add widgets (buttons, labels, etc.)
+    label_style = {
+        "text": "YOU MP3",
+        "bg": "#141414",          # Background color
+        "fg": "white",          # Text color
+        "font": ("Arial", 12),  # Font family and size
+        "width": 15,            # Button width
+        "height": 1,             # Button height
+        "font": "Lato"
+    }
+    label = tk.Label(root, label_style)
+    label.pack(side="top", padx=10, pady=10)
+    button_style = {
+        "text": "Download MP3",
+        "bg": "#c00707",          # Background color
+        "fg": "white",          # Text color
+        "font": ("Arial", 12),  # Font family and size
+        "width": 15,            # Button width
+        "height": 1,             # Button height
+        "relief": "flat",
+        "font": "Lato"
+        
+    }
+    button = tk.Button(root, **button_style)
+    button.configure(bg="#c00707")
+    button.pack()
+
+    # Start the GUI event loop
+    root.mainloop()
+    
