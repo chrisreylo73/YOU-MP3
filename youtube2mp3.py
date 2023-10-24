7ca8f23c26fefb4c356cfe5cec693632510f7fa3
chrisreylo73 <chrisreylo73@gmail.com>
2023-10-24 15:11:43 -0600

    Deletes video after conversion

--- youtube2mp3.py
+++ youtube2mp3.py
@@ -2,13 +2,14 @@ import time
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
 import os
+import shutil
 
 
 def download_youtube_video(url, video_folder_path):
     yt = YouTube(url)
     videoTitle = yt.title
     stream = yt.streams.filter(only_audio=False, file_extension='mp4').first()
-    stream.download(video_folder_path)
+    stream.download(video_folder_path);
     return videoTitle
 
 def extract_audio(input_path, output_audio_path):
@@ -16,18 +17,13 @@ def extract_audio(input_path, output_audio_path):
     audio = video.audio
     audio.write_audiofile(output_audio_path, codec="mp3")
     audio.close()
+    video.close()
 
-def delete_video(video_folder_path):
-    files = os.listdir(video_folder_path)
-    if files:
-        file_to_delete = os.path.join(video_folder_path, files[0])
-        try:
-            os.remove(file_to_delete)
-            print(f"Deleted file: {file_to_delete}")
-        except OSError as e:
-            print(f"Error removing file: {e}")
-    else:
-        print("No files to delete.")
+def delete_video(video_path):
+    try:
+        os.remove(video_path)
+    except OSError as e:
+        print(f"Error removing file: {e}")
        
 if __name__ == "__main__":
     home = "https://www.youtube.com/watch?v=iB3K1f7CFbo"
@@ -37,4 +33,4 @@ if __name__ == "__main__":
     output_audio_path = f"./Audio/{videoTitle}.mp3"
     extract_audio(f"{video_folder_path}/{videoTitle}.mp4", output_audio_path)
     time.sleep(3)
-    delete_video(video_folder_path)
+    delete_video(f"{video_folder_path}/{videoTitle}.mp4")
